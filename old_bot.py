7f97022aaecc2a441665c3cd13d17de7f0edf1ed
Tim <116808246+omlord10@users.noreply.github.com>
2023-03-26 22:35:32 +1000

    Update old_bot.py

--- old_bot.py
+++ old_bot.py
@@ -1,6 +1,6 @@
 # -*- coding: utf8 -*-
 import telebot
-bot = telebot.TeleBot('5977947434:AAGsNPomvqdNi_lLD0eo1wS0F5IFwR6bGzQ')
+bot = telebot.TeleBot('TOKEN')
 @bot.message_handler(commands=['start','Start','Старт','help', 'Help','back','Назад','Классы','Мем','Site', 'site', 'website', 'сайт', 'Сайт'])
 def start(message):
     bot.send_message(message.chat.id,'Привет дорогой друг, мы перешли на нового бота.\nhttps://t.me/Math_Lyceum_bot')
